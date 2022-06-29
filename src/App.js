60da32c9e8dbbe2b9c1f4d02e94961450b1a970c
jorislimonier <joris.limonier@hotmail.fr>
2022-06-29 02:52:24 +0200

    Add Navbar and social media icons

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <h1 className="text-2xl font-bold">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="bg-[#0A192F] w-full h-screen">
+      <HiArrowNarrowRight />
+    </div>
+  );
+};
+
+export default Home;
