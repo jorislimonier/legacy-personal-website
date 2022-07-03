7569f78c8429c8049454f827c8c31c80dbcbf77e
jorislimonier <joris.limonier@hotmail.fr>
2022-07-03 08:34:19 +0200

    Try deploy to heroku

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Joris LIMONIER
+          Joris LIMONIER test3
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           Mathematician and Data Scientist
