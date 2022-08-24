6093b48c123d0f1d305b83f8a2e22994d3280eaf
jorislimonier <joris.limonier@hotmail.fr>
2022-08-24 07:45:26 +0200

    Update project 2

--- src/components/Projects/Project02.tsx
+++ src/components/Projects/Project02.tsx
@@ -9,8 +9,8 @@ const Project1 = () => {
       </p>
       <h2 className="project__h2">Birth Date</h2>
       <p className="project__paragraph">
-        The birth date is the easiest of the three values computed. You start
-        with, say, <span>February 12, 1998</span>. Write it in number format:
+        The birth date is the easiest of the three values computed. My birthdate
+        is <span>February 12, 1998</span>. Write it in number format:
         12/02/1998, then sum up all its digits: 1 + 2 + 0 + 2 + 1 + 9 + 9 + 8 =
         32. If the result contains only one digit, stop. Otherwise, add its
         digits together. Since 32 contains two digits, we sum them up: 3 + 2 =
